1dc3aab1ca15482dd9910c8f6e6043d4564fc755
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 12:47:19 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,20 @@
 console.log("test");
 
+/* 
+Frage
+
+array mit schleife 0-10 befüllen
+
+übergabe mit push
+
+ausgabe numArray
+*/
+
+
+let Array =[];
+
+for ( runden = 0 ; runden <11 ; runden = runden + 1) {
+    let numArray = Array.push(runden);
+    console.log(numArray);
+    console.log(Array);
+}
